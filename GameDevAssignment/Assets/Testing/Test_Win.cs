978e385166038d8a9f815fcd4beb1a24eae242ca
nebulousmango <78063147+nebulousmango@users.noreply.github.com>
2021-04-24 20:43:12 +0530

    Changes

--- GameDevAssignment/Assets/Testing/Test_Win.cs
+++ GameDevAssignment/Assets/Testing/Test_Win.cs
@@ -3,14 +3,6 @@ public class Test_Win : MonoBehaviour
 {
     public UpgradePlayer Upgrade;
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.GetComponent<Test_PlayerContr>() == true)
-        {
-            Debug.Log("Please press space.");
-        }
-    }
-
     //Function for if player passes through this object. 
     private void OnTriggerStay2D(Collider2D other)
     {
@@ -19,7 +11,6 @@ public class Test_Win : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                Debug.Log("You have levelled up!");
                 Upgrade.ChangePlayer();
             }
         }
