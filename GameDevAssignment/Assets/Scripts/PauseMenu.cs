612ba3e3bd493372032dcc4ffcd5a3c410f7a459
nebulousmango <sitaradaniel@gmail.com>
2021-08-20 23:43:05 +0530

    Windows build update

--- GameDevAssignment/Assets/Scripts/PauseMenu.cs
+++ GameDevAssignment/Assets/Scripts/PauseMenu.cs
@@ -14,7 +14,7 @@ public class PauseMenu : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
             if (GameIsPaused)
             {
